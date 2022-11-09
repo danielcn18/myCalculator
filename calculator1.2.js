67b10dfcdf7a8ff54ebe50c6c7a62c0198e390d2
Daniel Chiquete <115588244+danielcn18@users.noreply.github.com>
2022-11-09 16:28:24 -0700

    Add files via upload

---
+++ calculator1.2.js
@@ -0,0 +1,23 @@
+var myValue;
+
+function clearArea(){
+    document.getElementById('display').value = null; 
+}
+
+function opt(myValue){
+    document.getElementById('display').value += myValue;
+}
+
+function posNegative(myValue){
+    document.getElementById('display').value *= myValue;
+}
+
+function percent(myValue){
+    var myValue = document.getElementById('display').value;
+    document.getElementById('display').value = myValue * 0.01;
+}
+
+function solve(){
+    var myValue = eval(document.getElementById('display').value);
+    document.getElementById('display').value = myValue;
+}
